62268b04742995a08f74917abd734ba295c33815
ArnoldBova <a03bova@siena.edu>
2022-04-25 15:12:57 -0400

    Implemented populating an array in board and changed tile structure

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,5 +1,104 @@
 package src.game;
 
-public class Board {
+import src.pieces.*;
+import src.structures.Piece;
 
+/**
+ * Implementation of an object representing a board. Will be the overall
+ * structure that contains all of the
+ * moving game components
+ */
+public class Board {
+    Tile[][] tiles;
+
+    /**
+     * constructs a new board, populating the 2d array of tile objects with each
+     * piece in the correct place to start a new game.
+     * 
+     * The board will be oriented such that the white piece player will be on the
+     * bottom of the screen.
+     */
+    public Board() {
+        tiles = new Tile[8][8];
+    }
+
+    private void populate() {
+        for (int i = 0; i < tiles.length; i++) {
+            switch (i) {
+                // generating black back line
+                case 0:
+                    for (int j = 0; j < tiles[i].length; j++) {
+                        String color = (j % 2 == 0) ? "white" : "black";
+                        tiles[i][j] = createTile(j, color, "black");
+                    }
+                    break;
+                // Generating black pawn line
+                case 1:
+                    for (int j = 0; j < tiles[i].length; j++) {
+                        String color = (j % 2 == 0) ? "black" : "white";
+                        Tile tile = new Tile(color);
+                        Piece piece = new Pawn(tile, "black");
+                        tile.setPiece(piece);
+                    }
+                    break;
+                case 2:
+                case 4:
+                    for (int j = 0; j < tiles[i].length; j++) {
+                        String color = (j % 2 == 0) ? "white" : "black";
+                        tiles[i][j] = new Tile(color);
+                    }
+                    break;
+                case 3:
+                case 5:
+                    for (int j = 0; j < tiles[i].length; j++) {
+                        String color = (j % 2 == 0) ? "black" : "white";
+                        tiles[i][j] = new Tile(color);
+                    }
+                    break;
+                case 6:
+                    for (int j = 0; j < tiles[i].length; j++) {
+                        String color = (j % 2 == 0) ? "white" : "black";
+                        Tile tile = new Tile(color);
+                        Piece piece = new Pawn(tile, "white");
+                        tile.setPiece(piece);
+                    }
+                    break;
+                case 7:
+                    for (int j = 0; j < tiles[i].length; j++) {
+                        String color = (j % 2 == 0) ? "black" : "white";
+                        tiles[i][j] = createTile(j, color, "white");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private Tile createTile(int pos, String tileColor, String pieceColor) {
+        Piece piece;
+        Tile tile = new Tile(tileColor);
+        // determine which piece should be generated
+        piece = switch (pos) {
+            // rook case
+            case 0 -> new Rook(tile, pieceColor);
+            // Knight case
+            case 1 -> new Knight(tile, pieceColor);
+            // Bishop case
+            case 2 -> new Bishop(tile, pieceColor);
+            // Queen case
+            case 3 -> new Queen(tile, pieceColor);
+            // King case
+            case 4 -> new King(tile, pieceColor);
+            // rest of the case are the same just mirrored;
+            case 5 -> new Bishop(tile, pieceColor);
+            case 6 -> new Knight(tile, pieceColor);
+            case 7 -> new Rook(tile, pieceColor);
+            default -> null;
+        };
+
+        tile.setPiece(piece);
+
+        return tile;
+    }
 }
--- src/game/Tile.java
+++ src/game/Tile.java
@@ -6,8 +6,13 @@ public class Tile {
     private Piece piece;
     private String color;
 
-    public Tile(Piece piece, String color) {
-        this.piece = piece;
+    public Tile(String color) {
+        this.piece = null;
         this.color = color;
     }
+
+    void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
 }

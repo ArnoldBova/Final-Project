84257d66fb3c47d80fc7327853040897e5ee956c
Emma Flatland <el25flat@siena.edu>
2022-04-26 15:17:28 -0400

    Add basic functionality to the Timer

--- src/game/Timer.java
+++ src/game/Timer.java
@@ -1,22 +1,42 @@
 package src.game;
 
 public class Timer extends Thread {
-    // True if the timer is stopped, and false if it is running
-    boolean stopped;
+    // True only if the timer is running
+    boolean running;
 
-    // True if the game is over, and false if the game is in progress
-    boolean gameOver;
-
-    // The amount of time the timer can run before the game is over
+    // The amount of time (in seconds) the timer can run before the game is over
     int gameLength;
+
+    // True if the timer is on, and false if it is off
+    boolean on;
 
     // The time that the timer will display (how long the player has spent on their
     // turns so far)
     int timeElapsed;
 
+    // The number of milliseconds the program waits before changing animation frames
+    public static final int DELAY_TIME = 1000;
+
+    public Timer(int gameLength) {
+        this.timeElapsed = 0;
+        this.running = false;
+        this.gameLength = gameLength;
+    }
+
     // Keeps track of the current time, and updates the elapsed time every second
     public void run() {
-
+        while (on) {
+            if (running) {
+                try {
+                    sleep(DELAY_TIME);
+                    timeElapsed++;
+                } catch (InterruptedException e) {
+                }
+            }
+            if (timeElapsed == gameLength) {
+                running = false;
+            }
+        }
     }
 
     // Paints the current elapsed time
@@ -26,7 +46,28 @@ public class Timer extends Thread {
 
     // Resets the elapsed time to zero
     public void reset() {
+        this.timeElapsed = 0;
 
+    }
+
+    // Temporarily pauses the timer
+    public void pause() {
+        this.running = false;
+    }
+
+    // Starts the timer
+    public void start() {
+        this.running = true;
+    }
+
+    // Turns the timer on
+    public void turnOn() {
+        this.on = true;
+    }
+
+    // Turns the timer off
+    public void turnOff() {
+        this.on = false;
     }
 
     // Converts a time in seconds to its equivalent time in minutes and seconds

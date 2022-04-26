22f1b242490b229dfd63c83bcff7e6806f5ff82b
Emma Flatland <el25flat@siena.edu>
2022-04-26 16:27:28 -0400

    Add constructor

--- src/game/Player.java
+++ src/game/Player.java
@@ -6,6 +6,9 @@ public class Player {
     // Indicates if the player has lost
     protected boolean hasLost;
 
+    // Indicates if the player has won
+    protected boolean hasWon;
+
     // This player's pieces
     protected Piece[] pieces;
 
@@ -15,14 +18,45 @@ public class Player {
     // The timer that tracks how much time this player has used
     Timer timer;
 
-    // Returns true if the player has lost, and false otherwise
+    /**
+     * Constructs a new Player
+     * 
+     * @param board  The board the player will use
+     * @param pieces The pieces the player will use
+     * @param timer  The timer the player will use
+     */
+    public Player(Board board, Piece[] pieces, Timer timer) {
+        this.board = board;
+        this.pieces = pieces;
+        this.timer = timer;
+
+        this.hasWon = false;
+        this.hasLost = false;
+    }
+
+    /**
+     * Returns true if the player has lost, and false otherwise
+     * 
+     * @return Whether the player has lost
+     */
     public boolean hasLost() {
         return hasLost;
     }
 
-    // Moves one of the player's pieces to a new location on the board
-    // (This could be a random piece, or we could make subclasses that play moves at
-    // varying levels of difficulty)
+    /**
+     * Returns true if the player has won
+     * 
+     * @return Whether the player has won
+     */
+    public boolean hasWon() {
+        return hasWon;
+    }
+
+    /**
+     * Moves one of the player's pieces to a new location on the board
+     * (This could be a random piece, or we could make subclasses that play moves at
+     * varying levels of difficulty)
+     */
     public void movePiece() {
 
     }

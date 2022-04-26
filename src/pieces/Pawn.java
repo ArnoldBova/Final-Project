decf29a8a7bea0bfacb324c989c6ead961db76e1
ArnoldBova <a03bova@siena.edu>
2022-04-25 21:50:38 -0400

    Implemented pawn valid move logic

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -4,17 +4,91 @@ import src.game.Tile;
 import src.structures.Piece;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Pawn extends Piece {
 
-    public Pawn(Tile tile, String color) {
-        super(tile, color);
+    boolean hasMoved;
+
+    public Pawn(Tile tile, boolean isWhite) {
+        super(tile, isWhite);
+        hasMoved = false;
     }
 
     @Override
     public ArrayList<Tile> getValidMoves() {
-        return null;
+        ArrayList<Tile> outcomes = new ArrayList<>();
+
+        Tile captureTileRight;
+        Tile captureTileLeft;
+        Tile nonCaptureTile1 = null;
+        Tile nonCpatureTile2 = null;
+
+        if (isWhite) {
+
+            captureTileLeft = tile.left();
+            if (captureTileLeft != null) {
+                captureTileLeft = captureTileLeft.up();
+            }
+
+            captureTileRight = tile.right();
+            if (captureTileRight != null) {
+                captureTileRight = captureTileRight.up();
+            }
+
+            if (captureTileLeft != null && captureTileLeft.hasPiece()) {
+                outcomes.add(captureTileLeft);
+            }
+
+            if (captureTileRight != null && captureTileRight.hasPiece()) {
+                outcomes.add(captureTileRight);
+            }
+
+            if (tile.up() != null) {
+                nonCaptureTile1 = tile.up();
+            }
+
+            if (!hasMoved && nonCaptureTile1 != null && !nonCaptureTile1.hasPiece()) {
+                nonCpatureTile2 = nonCaptureTile1.up();
+                outcomes.add(nonCaptureTile1);
+            }
+
+            if (nonCpatureTile2 != null && !nonCpatureTile2.hasPiece()) {
+                outcomes.add(nonCaptureTile1);
+            }
+        } else {
+            captureTileLeft = tile.left();
+            if (captureTileLeft != null) {
+                captureTileLeft = captureTileLeft.down();
+            }
+
+            captureTileRight = tile.right();
+            if (captureTileRight != null) {
+                captureTileRight = captureTileRight.down();
+            }
+
+            if (captureTileLeft != null && captureTileLeft.hasPiece()) {
+                outcomes.add(captureTileLeft);
+            }
+
+            if (captureTileRight != null && captureTileRight.hasPiece()) {
+                outcomes.add(captureTileRight);
+            }
+
+            if (tile.down() != null) {
+                nonCaptureTile1 = tile.down();
+            }
+
+            if (!hasMoved && nonCaptureTile1 != null && !nonCaptureTile1.hasPiece()) {
+                nonCpatureTile2 = nonCaptureTile1.down();
+                outcomes.add(nonCaptureTile1);
+            }
+
+            if (nonCpatureTile2 != null && !nonCpatureTile2.hasPiece()) {
+                outcomes.add(nonCaptureTile1);
+            }
+        }
+
+        return outcomes;
     }
 
     @Override

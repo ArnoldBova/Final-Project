56bb529a496336f42b0ebe5950d35c6f20692ad5
Ethan Tubia <ethantubia@Ethans-MacBook-Air.local>
2022-04-27 18:03:21 -0400

    Calculated moveList for the rook

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -14,7 +14,127 @@ public class Rook extends Piece {
 
     @Override
     public ArrayList<Tile> getValidMoves() {
-        return null;
+        ArrayList<Tile> outcomes = new ArrayList<>();
+
+        Tile captureTileRight;
+        Tile captureTileLeft;
+        Tile captureTileUp;
+        Tile captureTileDown;
+        Tile nonCaptureTile1 = null;
+        Tile nonCpatureTile2 = null;
+
+        captureTileLeft = tile.left();
+        captureTileRight = tile.right();
+        boolean wall = false;
+        if(isWhite){
+        while(captureTileLeft != null && wall == false){
+            if(captureTileLeft.hasPiece() && captureTileLeft.piece().isWhite()!= true){
+                outcomes.add(captureTileLeft);
+                wall = true;
+            }else{
+                if(!captureTileLeft.hasPiece()){
+                    outcomes.add(captureTileLeft);
+                    captureTileLeft = captureTileLeft.left();
+                }else{
+                    wall = true;
+                }
+            }
+        }
+        wall = false;
+        while(captureTileRight != null && wall == false){
+            if(captureTileRight.hasPiece() && captureTileRight.piece().isWhite() != true){
+                outcomes.add(captureTileRight);
+                wall = true;
+            }else{
+                if(!captureTileRight.hasPiece()){
+                    outcomes.add(captureTileRight);
+                    captureTileRight = captureTileRight.right();
+                }
+            }
+        }
+        wall = false;
+        captureTileUp = tile.up();
+        while(captureTileUp != null && wall == false){
+            if(captureTileUp.hasPiece() && captureTileUp.piece().isWhite() != true){
+                outcomes.add(captureTileUp);
+                wall = true;
+            }else{
+                if(!captureTileUp.hasPiece()){
+                    outcomes.add(captureTileUp);
+                    captureTileUp = captureTileUp.up();
+                }
+            }
+        }
+        wall = false;
+        captureTileDown = tile.down();
+        while(captureTileDown != null && wall == false){
+            if(captureTileDown.hasPiece() && captureTileDown.piece().isWhite() != true){
+                outcomes.add(captureTileDown);
+                wall = true;
+            }else{
+                if(!captureTileDown.hasPiece()){
+                    outcomes.add(captureTileDown);
+                    captureTileDown = captureTileDown.down();
+                }
+            }
+        }
+    }else{
+    while(captureTileLeft != null && wall == false){
+        if(captureTileLeft.hasPiece() && captureTileLeft.piece().isWhite()){
+            outcomes.add(captureTileLeft);
+            wall = true;
+        }else{
+            if(!captureTileLeft.hasPiece()){
+                outcomes.add(captureTileLeft);
+                captureTileLeft = captureTileLeft.left();
+            }else{
+                wall = true;
+            }
+        }
+    }
+    wall = false;
+    while(captureTileRight != null && wall == false){
+        if(captureTileRight.hasPiece() && captureTileRight.piece().isWhite()){
+            outcomes.add(captureTileRight);
+            wall = true;
+        }else{
+            if(!captureTileRight.hasPiece()){
+                outcomes.add(captureTileRight);
+                captureTileRight = captureTileRight.right();
+            }
+        }
+    }
+    wall = false;
+    captureTileUp = tile.up();
+    while(captureTileUp != null && wall == false){
+        if(captureTileUp.hasPiece() && captureTileUp.piece().isWhite()){
+            outcomes.add(captureTileUp);
+            wall = true;
+        }else{
+            if(!captureTileUp.hasPiece()){
+                outcomes.add(captureTileUp);
+                captureTileUp = captureTileUp.up();
+            }
+        }
+    }
+    wall = false;
+    captureTileDown = tile.down();
+    while(captureTileDown != null && wall == false){
+        if(captureTileDown.hasPiece() && captureTileDown.piece().isWhite()){
+            outcomes.add(captureTileDown);
+            wall = true;
+        }else{
+            if(!captureTileDown.hasPiece()){
+                outcomes.add(captureTileDown);
+                captureTileDown = captureTileDown.down();
+            }
+        }
+    }
+}
+
+        
+
+        return outcomes;
     }
 
     @Override

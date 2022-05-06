22a3b5dd8d897eacba36fe06cf8987a4de4dfa42
Ethan T <90419836+ej16tubi@users.noreply.github.com>
2022-05-05 22:52:21 -0400

    Update Piece.java
    Added Javadoc comments to methods that didn't have them

--- src/structures/Piece.java
+++ src/structures/Piece.java
@@ -27,32 +27,76 @@ public abstract class Piece extends Thread implements ImageObserver {
 
     protected Point position;
 
+    /**
+    *
+    *constructs a new piece
+    *
+    *@param tile is where the piece is
+    *
+    *@param isWhite is the color of the piece
+    */
+    
     public Piece(Tile tile, boolean isWhite, JComponent container) {
         this.tile = tile;
         this.isWhite = isWhite;
         this.container = container;
         position = new Point(tile.location().x * 50 + 40, tile.location().y * 50);
     }
-
-    // retuns an array of tiles representing the end point for all valid moves for
-    // that piece
+    /**
+    * retuns an array of tiles representing the end point for all valid moves for
+    * that piece
+    * @return ArrayList of tiles that are valid moves for the piece
+    */
     public abstract ArrayList<Tile> getValidMoves();
 
+    /**
+    *returns the tile instance variable 
+    *@return tile
+    */
+    
     public Tile getTile() {
         return tile;
     }
 
+    /**
+    *
+    *sets the value to the tile instance variable
+    *
+    *@param tile is the value being assigned to the instance 
+    *variable tile
+    */
+    
     public void setTile(Tile tile) {
         this.tile = tile;
     }
 
+    /**
+    *returns true if the piece is white
+    *and false if the piece is black
+    *
+    *@returns boolean value stored in isWhite instance variable
+    */
+    
     public boolean isWhite() {
         return isWhite;
     }
+    
+    /**
+    *returns true if the piece is captured
+    *and false if not.
+    *
+    *@returns  boolean the value stored in captured instance variable
+    */
 
     public boolean isCaptured() {
         return captured;
     }
+    
+    /**
+    *returns the position of the piece
+    *
+    *@return Point that the piece is at
+    */
 
     public Point getPosition(){
         return position;
@@ -90,6 +134,12 @@ public abstract class Piece extends Thread implements ImageObserver {
 
     };
 
+    /**
+    *Required method by the ImageObserver
+    *ensures the image for the piece is loaded and ready
+    *to be drawn to the screen
+    */
+    
     @Override
     public boolean imageUpdate(Image img, int infoflags, int x, int y,
                                int width, int height) {
@@ -101,6 +151,11 @@ public abstract class Piece extends Thread implements ImageObserver {
         return true;
     }
 
+    /**
+    * sets the captured instance
+    * variable to true
+    */
+    
     public void setCapture(){
         this.captured = true;
     }

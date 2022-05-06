d9c46c1a63278e24639e7fbcd85e95f17163a16f
Ethan T <90419836+ej16tubi@users.noreply.github.com>
2022-05-05 22:35:37 -0400

    Update Player.java
    Added Javadoc comments to methods that did not have them

--- src/game/Player.java
+++ src/game/Player.java
@@ -5,26 +5,68 @@ import src.structures.Piece;
 public class Player {
     boolean inCheck;
     boolean isWhite;
-
+    
+    /**
+    * Constructs a player and assigns
+    * the player a color of piece
+    * @param isWhite a boolean that determines color
+    */
+    
     public Player(boolean isWhite) {
         inCheck = false;
         this.isWhite = isWhite;
     }
+    
+    /**
+    *returns the value stored in
+    *isWhite
+    *@return isWhite a boolean value that is true 
+    *when the player has the white pieces
+    */
 
     public boolean isWhite() {
         return isWhite;
     }
-
+    
+    /**
+    *returns the value stored in 
+    *inCheck
+    *@return inCheck a boolean value that
+    *is true when the player's king is in
+    *check
+    */
+    
     public boolean isInCheck() {
         return inCheck;
     }
+    
+    /**
+    *sets the inCheck instance
+    *variable for the player it
+    *is called on to true.
+    */
+    
     public void inCheck () {
         inCheck = true;
     }
+    
+    /**
+    *sets the inCheck instance
+    *variable for the player it
+    *is called on to false.
+    */
+    
     public void outOfCheck() {
         inCheck = false;
     }
-
+    
+    /**
+    *returns a the color the player
+    *it is called on in a string
+    *@returns String consisting of
+    *the player's color, and the word player
+    */
+    
     @Override
     public String toString(){
         if(isWhite){
